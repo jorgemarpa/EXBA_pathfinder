8e1e9e4c799b4c26a5abd01632da5422b738e24c
Jorge <jorgemarpa@ug.uchile.cl>
2021-02-03 23:07:48 -0800

    :sparkles: exba lc collection class and do bls search for all Q

--- exba/exba_tools.py
+++ exba/exba_tools.py
@@ -16,7 +16,7 @@ from astropy.time import Time
 from astropy.coordinates import SkyCoord, match_coordinates_3d
 from astropy.stats import sigma_clip
 
-from .utils import get_gaia_sources, fap_bootstrap
+from .utils import get_gaia_sources, get_bls_periods
 
 main_path = os.path.dirname(os.getcwd())
 
@@ -91,7 +91,7 @@ class EXBA(object):
 
         # search Gaia sources in the sky
         sources = self._get_coord_and_query_gaia(
-            self.ra, self.dec, self.unw, self.time[0], magnitude_limit=25
+            self.ra, self.dec, self.unw, self.time[0], magnitude_limit=20
         )
         sources["col"], sources["row"] = tpfs[0].wcs.wcs_world2pix(
             sources.ra, sources.dec, 0.5
@@ -271,11 +271,37 @@ class EXBA(object):
                 & (np.abs(self.row - np.floor(s.row)) < aper[0])
                 for _, s in self.sources.iterrows()
             ]
+        elif space == "pix-auto":
+            if not hasattr(self, "radius"):
+                raise AttributeError(
+                    "No PSF radius computed, please run `_find_psf_edge()` first"
+                )
+            # create circular aperture mask using PSF edges
+            aperture_mask = [
+                np.hypot(self.col - s.col, self.row - s.row) <= r
+                for r, (_, s) in zip(self.radius, self.sources.iterrows())
+            ]
+            # create a background mask
+            sq_ap = [
+                (np.abs(self.col - np.floor(s.col)) < 6)
+                & (np.abs(self.row - np.floor(s.row)) < 6)
+                for _, s in self.sources.iterrows()
+            ]
+            bkg_mask = np.asarray(sq_ap).sum(axis=0) == 0
+            self.bkg_mask = bkg_mask.reshape(self.col_2d.shape)
+            # compute a SNR image
+            mean_flux = self.flux.value.mean(axis=0)
+            bkg_std = mean_flux[bkg_mask].std()
+            snr_img = np.abs(mean_flux / bkg_std)
+            self.snr_img = snr_img.reshape(self.col_2d.shape)
+            # combine circular aperture with SNR > 5 mask
+            aperture_mask &= snr_img > 3
 
+        aperture_mask = np.asarray(aperture_mask)
         sap = np.zeros((self.sources.shape[0], self.flux.shape[0]))
         sap_e = np.zeros((self.sources.shape[0], self.flux.shape[0]))
 
-        for tdx in tqdm(range(len(self.flux)), desc="Simple SAP flux"):
+        for tdx in tqdm(range(len(self.flux)), desc="Simple SAP flux", leave=False):
             sap[:, tdx] = [self.flux[tdx][mask].value.sum() for mask in aperture_mask]
             sap_e[:, tdx] = [
                 np.power(self.flux_err[tdx][mask].value, 2).sum() ** 0.5
@@ -283,14 +309,14 @@ class EXBA(object):
             ]
 
         # check for light curves with zero flux
-        zero_mask = np.all((sap == 0), axis=1)
-        sap = sap[~zero_mask]
-        sap_e = sap_e[~zero_mask]
-        aperture_mask = np.asarray(aperture_mask)[~zero_mask]
-        self.bad_sources = pd.concat(
-            [self.bad_sources, self.sources[zero_mask]]
-        ).reset_index(drop=True)
-        self.sources = self.sources[~zero_mask].reset_index(drop=True)
+        # zero_mask = np.all((sap == 0), axis=1)
+        # sap = sap[~zero_mask]
+        # sap_e = sap_e[~zero_mask]
+        # aperture_mask = aperture_mask[~zero_mask]
+        # self.bad_sources = pd.concat(
+        #     [self.bad_sources, self.sources[zero_mask]]
+        # ).reset_index(drop=True)
+        # self.sources = self.sources[~zero_mask].reset_index(drop=True)
 
         self.aperture_mask = aperture_mask.reshape(
             self.sources.shape[0], self.flux_2d.shape[1], self.flux_2d.shape[2]
@@ -307,8 +333,8 @@ class EXBA(object):
                     targetid=self.sources.designation[i],
                     label=self.sources.designation[i],
                     mission="Kepler",
-                    quarter=self.quarter,
-                    channel=self.channel,
+                    quarter=int(self.quarter),
+                    channel=int(self.channel),
                     ra=self.sources.ra[i],
                     dec=self.sources.dec[i],
                 ).remove_outliers(sigma=5)
@@ -317,16 +343,16 @@ class EXBA(object):
         )
         return
 
-    def _find_psf_edge(self, radius_limit=20, cut=300, plot=True):
+    def _find_psf_edge(self, radius_limit=6, cut=300, plot=True):
 
         mean_flux = np.nanmean(self.flux, axis=0).value
         r = np.hypot(self.dx, self.dy)
 
-        temp_mask = (r < radius_limit) & (self.gf < 1e7)
+        temp_mask = (r < radius_limit) & (self.gf < 1e6) & (self.gf > 1e4)
         temp_mask &= temp_mask.sum(axis=0) == 1
 
-        f = np.log10((temp_mask.astype(float) * mean_flux))
-        print(f.shape)
+        with np.errstate(divide="ignore", invalid="ignore"):
+            f = np.log10((temp_mask.astype(float) * mean_flux))
         weights = (
             (self.flux_err ** 0.5).sum(axis=0) ** 0.5 / self.flux.shape[0]
         ) * temp_mask
@@ -336,11 +362,11 @@ class EXBA(object):
                 r[temp_mask],
                 r[temp_mask] ** 2,
                 np.log10(self.gf[temp_mask]),
-                np.log10(self.gf[temp_mask]) ** 2,
+                # np.log10(self.gf[temp_mask]) ** 2,
             ]
         ).T
         k = np.isfinite(f[temp_mask])
-        for count in [0, 1, 2]:
+        for count in [0]:
             sigma_w_inv = A[k].T.dot(A[k] / weights[temp_mask][k, None] ** 2)
             B = A[k].T.dot(f[temp_mask][k] / weights[temp_mask][k] ** 2)
             w = np.linalg.solve(sigma_w_inv, B)
@@ -362,7 +388,7 @@ class EXBA(object):
                     test_r2.ravel(),
                     test_r2.ravel() ** 2,
                     test_f2.ravel(),
-                    test_f2.ravel() ** 2,
+                    # test_f2.ravel() ** 2,
                 ]
             )
             .T.dot(w)
@@ -381,20 +407,13 @@ class EXBA(object):
         )
         source_radius_limit[source_radius_limit > radius_limit] = radius_limit
         self.radius = source_radius_limit
-        self.source_mask = sparse.csr_matrix(
-            self.r.to("arcsec").value < self.radius[:, None]
-        )
 
         if plot:
             fig, ax = plt.subplots(1, 2, figsize=(14, 5), facecolor="white")
 
-            ax[0].scatter(
-                r.value[temp_mask][k], f[temp_mask][k], s=0.4, c="k", label="Data"
-            )
-            ax[0].scatter(
-                r.value[temp_mask][k], A[k].dot(w), c="r", s=0.4, label="Model"
-            )
-            ax[0].set(xlabel=("Radius [arcsec]"), ylabel=("log$_{10}$ Flux"))
+            ax[0].scatter(r[temp_mask][k], f[temp_mask][k], s=0.4, c="k", label="Data")
+            ax[0].scatter(r[temp_mask][k], A[k].dot(w), c="r", s=0.4, label="Model")
+            ax[0].set(xlabel=("Radius [pix]"), ylabel=("log$_{10}$ Flux"))
             ax[0].legend(frameon=True)
 
             im = ax[1].pcolormesh(
@@ -414,7 +433,7 @@ class EXBA(object):
             cbar.set_label("Contained PSF Flux [counts]")
 
             ax[1].set(
-                ylabel=("Radius from Source [arcsecond]"),
+                ylabel=("Radius from Source [pix]"),
                 xlabel=("log$_{10}$ Source Flux"),
             )
             plt.show()
@@ -423,6 +442,7 @@ class EXBA(object):
     def apply_CBV(self, do_under=False, ignore_warnings=True):
         if ignore_warnings:
             warnings.filterwarnings("ignore", category=RuntimeWarning)
+            warnings.filterwarnings("ignore", category=lk.utils.LightkurveWarning)
 
         # Select which CBVs to use in the correction
         cbv_type = ["SingleScale"]
@@ -434,7 +454,7 @@ class EXBA(object):
         under_fit_m = []
         corrected_lcs = []
 
-        for lc in tqdm(self.sap_lcs, desc="Applying CBVs to LCs"):
+        for lc in tqdm(self.sap_lcs, desc="Applying CBVs to LCs", leave=False):
             lc = lc[lc.flux_err > 0]
             cbvcor = CBVCorrector(lc)
             cbvcor.correct_gaussian_prior(
@@ -477,7 +497,7 @@ class EXBA(object):
 
         return
 
-    def do_bls_search(self, plot=False, test_lcs=None):
+    def do_bls_search(self, plot=False, test_lcs=None, n_boots=100):
 
         if not hasattr(self, "corrected_lcs"):
             raise AttributeError(
@@ -488,61 +508,17 @@ class EXBA(object):
         else:
             search_here = self.corrected_lcs
 
-        search_period = np.arange(0, 25, 0.2)[1:]
-        period_best, period_fap = [], []
-        pmin, pmax, ffac = 0.5, 20, 10
+        period_best, period_fap, power_snr = get_bls_periods(
+            search_here, plot=plot, n_boots=n_boots
+        )
 
-        for lc in tqdm(search_here, desc="BLS search"):
-            lc = lc.remove_outliers(sigma_lower=20, sigma_upper=5)
-            periodogram = lc.to_periodogram(
-                method="bls",
-                minimum_period=pmin,
-                maximum_period=pmax,
-                frequency_factor=ffac,
-            )
-            best_fit_period = periodogram.period_at_max_power
-            p_fap = fap_bootstrap(
-                periodogram.power.max(),
-                pmin,
-                pmax,
-                ffac,
-                lc.time,
-                lc.flux,
-                lc.flux_err,
-                n_bootstraps=100,
-                random_seed=99,
-            )
-            period_best.append(best_fit_period)
-            period_fap.append(p_fap)
-
-            if plot:
-                fig = plt.figure(figsize=(13, 9))
-
-                plt.subplots_adjust(wspace=0.25, hspace=0.25)
-
-                sub1 = fig.add_subplot(2, 2, 1)
-                sub1.axvline(
-                    best_fit_period.value,
-                    color="r",
-                    linestyle="--",
-                    label="Period : %.3f d\nfap : %.3f"
-                    % (best_fit_period.value, p_fap),
-                )
-                periodogram.plot(ax=sub1)
-                sub1.legend()
-
-                sub2 = fig.add_subplot(2, 2, 2)
-                lc.fold(
-                    period=best_fit_period,
-                    epoch_time=periodogram.transit_time_at_max_power,
-                ).errorbar(ax=sub2, alpha=0.8)
-
-                sub3 = fig.add_subplot(2, 2, (3, 4))
-                lc.plot(ax=sub3)
-                periodogram.get_transit_model().plot(ax=sub3, color="r")
-                plt.show()
-
-        return u.Quantity(period_best), u.Quantity(period_fap)
+        if test_lcs:
+            return period_best, period_fap, power_snr
+        else:
+            self.periods = period_best
+            self.periods_fap = period_fap
+            self.periods_snr = power_snr
+        return
 
     def plot_image(self, space="pixels", sources=True, **kwargs):
         if space == "pixels":
@@ -607,7 +583,7 @@ class EXBA(object):
         for s in range(0, len(sources), step):
             if aperture_mask[s].sum() == 0:
                 print("Warning: zero pixels in aperture mask.")
-                # continue
+                continue
 
             fig, ax = plt.subplots(
                 1, 2, figsize=(15, 4), gridspec_kw={"width_ratios": [4, 1]}
@@ -757,3 +733,138 @@ class EXBACollection(EXBA):
         if do_under:
             self.under_fitting_metrics = np.asarray(under_fit_m, dtype=object)
         return
+
+
+class EXBALightCurveCollection:
+    def __init__(self, lcs, metadata):
+
+        # check if each element of exba_quarters are EXBA objects
+        # if not all([isinstance(exba, EXBA) for exba in EXBAs]):
+        #     raise AssertionError("All elements of the list must be EXBA objects")
+
+        self.channel = np.unique([lc[0].channel for lc in lcs])
+        self.quarter = np.unique([lc[0].quarter for lc in lcs])
+
+        # check that gaia sources are in all quarters
+        gids = [df.designation.tolist() for df in metadata]
+        unique_gids = np.unique([item for sublist in gids for item in sublist])
+
+        # create matris with index position to link sources across quarters
+        # this asume that sources aren't in the same position in the DF, sources
+        # can disapear (fall out the ccd), not all sources show up in all quarters.
+        pm = np.empty((len(unique_gids), len(lcs)), dtype=np.int) * np.nan
+        for q in range(len(lcs)):
+            mask1 = np.in1d(metadata[q].designation.values, unique_gids)
+            mask2 = np.in1d(unique_gids, metadata[q].designation.values)
+            pm[mask2, q] = np.arange(len(metadata[q].designation.values))[mask1]
+
+        # rearange sources as list of lk Collection containing all quarters per source
+        sources = []
+        for i, gid in enumerate(unique_gids):
+            aux = lk.LightCurveCollection(
+                [lcs[q][int(pos)] for q, pos in enumerate(pm[i]) if np.isfinite(pos)]
+            )
+            sources.append(lk.LightCurveCollection(aux))
+        self.lcs = sources
+        self.metadata = (
+            pd.concat(metadata, axis=0, join="outer")
+            .drop_duplicates(["designation"], ignore_index=True)
+            .drop(["Unnamed: 0", "col", "row"], axis=1)
+        )
+
+    def __repr__(self):
+        ch_result = ",".join([str(k) for k in list([self.channel])])
+        q_result = ",".join([str(k) for k in list([self.quarter])])
+        return (
+            "Light Curves from: \n\tChannel %s \n\tQuarter %s \n\tGaia sources %i"
+            % (
+                ch_result,
+                q_result,
+                len(self.lcs),
+            )
+        )
+
+    def stitch_quarters(self):
+
+        # lk.LightCurveCollection.stitch() normalize by default all lcs before stitching
+        if hasattr(self, "source_lcs"):
+            self.stitched_lcs = lk.LightCurveCollection(
+                [lc.stitch() for lc in self.source_lcs]
+            )
+
+        return
+
+    def do_bls_search(self, plot=False, n_boots=100, fap_tresh=0.1):
+
+        self.metadata["has_planet"] = False
+        self.metadata["N_periodic_quarters"] = 0
+        self.metadata["Period"] = None
+        self.metadata["Period_snr"] = None
+        self.metadata["Period_fap"] = None
+
+        for lc_long in tqdm(self.lcs, desc="Gaia sources"):
+
+            periods, faps, snrs = get_bls_periods(lc_long, plot=plot, n_boots=n_boots)
+            print(periods, faps, snrs)
+            # check for significant periodicity detection in at least one quarter
+            if np.isfinite(faps).all():
+                p_mask = faps < fap_tresh
+            else:
+                p_mask = snrs > 50
+            if p_mask.sum() > 0:
+                # check that periods are similar, within a tolerance
+                # this assumes that there's one only one period
+                # have to fix this to make it work for multiple true periods detected
+                # or if BLS detected ane of the armonics, not necesary yet, when need it
+                # use np.round(periods) and np.unique() to check for same periods and
+                # harmonics within tolerance.
+                good_periods = periods[p_mask]
+                good_faps = faps[p_mask]
+                good_snrs = snrs[p_mask]
+                all_close = (
+                    np.array(
+                        [np.isclose(p, good_periods, atol=0.1) for p in good_periods]
+                    ).sum(axis=0)
+                    > 1
+                )
+                if all_close.sum() > 1:
+                    idx = np.where(self.metadata.designation == lc_long[0].label)[0]
+                    self.metadata["has_planet"].iloc[idx] = True
+                    self.metadata["N_periodic_quarters"].iloc[idx] = all_close.sum()
+                    self.metadata["Period"].iloc[idx] = good_periods[all_close].mean()
+                    self.metadata["Period_fap"].iloc[idx] = good_faps[all_close].mean()
+                    self.metadata["Period_snr"].iloc[idx] = good_snrs[all_close].mean()
+                # check if periods are harmonics
+            break
+
+    @staticmethod
+    def from_stored_lcs(channels, quarters):
+
+        # load gaia catalogs and lcs
+        metadata, lcs, nodata = [], [], []
+        for ch in channels:
+            for q in quarters:
+                df_path = "%s/data/EXBA/%i/%i/gaia_dr2_xmatch.csv" % (main_path, ch, q)
+                lc_path = "%s/data/EXBA/%i/%i/sap_lcs_CBVcorrected.pkl" % (
+                    main_path,
+                    ch,
+                    q,
+                )
+                if not os.path.isfile(df_path) or not os.path.isfile(lc_path):
+                    print(
+                        "WARNING: channel %i quarter %i have no storaged files"
+                        % (ch, q)
+                    )
+                    nodata.append([ch, q])
+                    continue
+                df = pd.read_csv(df_path)
+                lc = cPickle.load(
+                    open(
+                        lc_path,
+                        "rb",
+                    )
+                )
+                metadata.append(df)
+                lcs.append(lc)
+
+        return EXBALightCurveCollection(lcs, metadata)
